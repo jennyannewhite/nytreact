e65efe16ca054325b1b185c2231d7087d363276c
Jennifer <white.jenniferann@gmail.com>
2018-05-03 14:35:17 -0500

    article list

---
+++ src/components/article_list_item.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ArticleListItem = (props) => {
+    const article = props.article;
+    const onArticleSelect = props.onArticleSelect;
+
+    // console.log(article.web_url);
+
+    const newsSource = article.source;
+    const headline = article.headline.main;
+    const articleURL = article.web_url; // to be included later
+
+    return ( <
+        li onClick = {
+            () => onArticleSelect(article) }
+        className = "list-group-item" >
+        <
+        div className = "article-list media" >
+        <
+        div className = "media-left" >
+        <
+        /div> <
+        div className = "media-body" >
+        <
+        div className = "media-heading" >
+        <
+        h6 > { headline } < /h6> <
+        /div> <
+        /div> <
+        /div> <
+        /li>
+    );
+};
+
+export default ArticleListItem;
